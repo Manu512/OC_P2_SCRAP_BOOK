e2443246d53f004c4a308c9778ad2a86ceee5728
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-01-24 00:55:40 +0100

    Phase 2 : Catégorie + MultiPages OK

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import requests
 import re
 import csv
 from bs4 import BeautifulSoup
-
-url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+url_base = 'http://books.toscrape.com/'
+# url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+# url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+url = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 
 
 def books(url_produit):
@@ -14,6 +16,7 @@ def books(url_produit):
     if response.ok:
         soup = BeautifulSoup(response.content, 'lxml')
         contenu = soup.find_all("table")[0].find_all("td")
+
         for cont in contenu:  # Parcours du tableau Information et ajout des infos dans la list info.
             info.append(cont.text)
 
@@ -24,21 +27,52 @@ def books(url_produit):
             "title": soup.find_all("div", class_="product_main")[0].h1.text,
             "price_including_tax": info[3],
             "price_excluding_tax": info[2],
-            "number_available":    re.search("\d.", info[5]).group(),
+            "number_available": re.search("\d.", info[5]).group(),
             "product_description": soup.select("#product_description ~ p")[0].text,
             "category": info[1],
             # Information N° etoile
             "review_rating": soup.find_all("p", class_="star-rating")[0].attrs['class'][1],
-            #TODO : Modifier le ../.. par la racine reel lors de l'itération
-            "image_url": soup.select("div.item.active")[0].img.attrs["src"]
+            "image_url": url_base + soup.select("div.item.active")[0].img.attrs["src"].replace("../../", "")
         }
 
 
-if __name__ == '__main__':
-    with open('extract/book.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
-        fieldnames = ['productpage_url', 'upc', 'title', 'price_including_tax',  'price_excluding_tax',
-                      'number_available', 'product_description', 'category', 'review_rating', 'image_url']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerow(books(url))
+def listing_category(url_category):
+    category = []
+    links = []
+    response = requests.get(url_category)
+    # Si Page Ok => On continue
+    if response.ok:
+        category.append(url_category)
+        category_base = url_category.replace("index.html", "")
+        soup = BeautifulSoup(response.content, 'lxml')
 
+# On cherche la class Next pour définir s'il y a plusieurs pages dans la catégorie, on sort de la boucle
+        while True:
+            if not soup.find('li', class_='next'):
+                break
+            response = requests.get(category_base + soup.find('li', class_='next').a['href'])
+            if response.ok:
+                category.append(response.url)
+                soup = BeautifulSoup(response.content, 'lxml')
+
+# Une fois toutes les pages récupérées on passe à la recuperation des livres
+    for chemin in category:
+        response = requests.get(chemin)
+        if response.ok:
+            soup = BeautifulSoup(response.content, 'lxml')
+            contenu = soup.find_all("article", class_="product_pod")
+            for cont in contenu:
+                link = cont.a['href']
+                links.append(url_base + "catalogue/" + link.replace("../../../", ""))
+
+    return links
+
+
+if __name__ == '__main__':
+    # with open('extract/book.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+    #     fieldnames = ['productpage_url', 'upc', 'title', 'price_including_tax',  'price_excluding_tax',
+    #                   'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     writer.writerow(books(url))
+    print(listing_category(url))
