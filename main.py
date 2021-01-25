e91bd0eddc28b33112a0307da13ee14316e8b643
WALDNER Emmanuel <emmanuel.waldner@gmail.com>
2021-01-25 10:18:31 +0100

    CORRECTION : La recuperation de la categorie etait erroné.

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def books(url_produit):
     # Si Page Ok => On continue
     if response.ok:
         soup = BeautifulSoup(response.content, 'lxml')
-        contenu = soup.find_all("table")[0].find_all("td")
-        for cont in contenu:  # Parcours du tableau Information et ajout des infos dans la list info.
-            info.append(cont.text)
+        contenus = soup.find_all("table")[0].find_all("td")
+        for contenu in contenus:  # Parcours du tableau Information et ajout des infos dans la list info.
+            info.append(contenu.text)
 
         return {
             "productpage_url": url_produit,
@@ -26,7 +26,7 @@ def books(url_produit):
             "price_excluding_tax": info[2],
             "number_available":    re.search("\d.", info[5]).group(),
             "product_description": soup.select("#product_description ~ p")[0].text,
-            "category": info[1],
+            "category": soup.find("ul",class_='breadcrumb').find_all("a")[2].text,
             # Information N° etoile
             "review_rating": soup.find_all("p", class_="star-rating")[0].attrs['class'][1],
             #TODO : Modifier le ../.. par la racine reel lors de l'itération
@@ -41,4 +41,5 @@ if __name__ == '__main__':
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerow(books(url))
+
 
